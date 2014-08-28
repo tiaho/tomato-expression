dff18f356ecaa1caff29423a24bababe5370d82a
Tiffany Ho <tiaho@ucdavis.edu>
2014-08-28 09:05:04 -0700

    initial version

---
+++ ui.R
@@ -0,0 +1,34 @@
+# ui.R
+
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Tomato Expression"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput("gene", label = h5("Enter desired gene name"), value = "Solyc02g081130.1.1"),
+      br(),
+      br(),
+      checkboxInput("logscale", "Plots in a log scale", value = TRUE),
+      br(),
+      radioButtons("table_options", label = h5("Display values in a table"),
+                         choices = list("Normalized CPM" = 1,
+                                        "log2(Normalized CPM)" = 2,
+                                        "FDR Corrected p-values for Overall Significance" = 3,
+                                        "FDR Corrected p-values for Pairwise Significance" = 4)),
+      br(),
+      textOutput("overall_significance"),
+      br(),
+      textOutput("pairwise_significance")
+    ),
+    
+    mainPanel(
+      plotOutput("graph"),
+      br(),
+      br(),
+      tableOutput("table")
+    )
+  )
+    
+))
