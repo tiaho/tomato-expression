7301e5d1ab02da354e197b369bb68cde45f61585
Tiffany Ho <tiaho@ucdavis.edu>
2015-02-03 18:52:34 -0800

    trying to get gene name trimming to work

--- server.R
+++ server.R
@@ -10,23 +10,58 @@ shinyServer(function(input, output) {
   
   # plots the graph
   output$graph <- renderPlot({
-    place_seedlings <- match(input$gene, seedlings$X, nomatch = 0)
+    places <- grep("input$gene", seedlings$X)
+    names <- vector()
+    for (i in 1:length(places)){
+      names[i] <- as.character(seedlings$X[places[i]])
+    }
+    place_seedlings <- match(names[1], seedlings$X, nomatch = 0)
     if (place_seedlings == 0){
-      stop(paste(input$gene, "does not exist"))
+      stop(paste(names[1], "does not exist"))
     }else{
-      Species <- names(seedlings)[2:5]
-      CPM <- as.numeric(seedlings[place_seedlings, 2:5])
-      if (input$logscale == TRUE) {CPM <- log2(CPM)}
-      combined <- cbind(Species, CPM)
+      # populates a group of vectors with the needed data for the genes found
+      combined <- vector() 
+      for (genes in names){
+        place_seedlings <- match(genes, seedlings$X, nomatch = 0)
+        Species <- names(seedlings)[2:5]
+        CPM <- as.numeric(seedlings[place_seedlings, 2:5])
+        gene_name <- rep(genes, 4)
+#         if (input$logscale == TRUE) {CPM <- log2(CPM)}
+        tmp <- cbind(Species, CPM, gene_name)
+        combined <- rbind(combined, tmp)
+      }
       df <- as.data.frame(combined)      
-      ggplot(df, aes(x = Species, y = CPM)) + 
+      
+      # makes a string of the gene names
+      gene_names_string <- character()
+      for (genes in names){
+        gene_names_string <- paste(gene_names_string, genes)
+      }
+
+      # plots
+      ggplot(df, aes(x = Species, y = as.numeric(as.character(CPM)))) + 
         geom_bar(stat = "identity", position = "identity", aes(fill = Species)) + 
-        ggtitle(paste("Expression level of", input$gene))
-#       qplot(x = Species, y = CPM, fill = Species,
-#             main = paste("Expression level of", input$gene)) + geom_bar(stat = "identity", position = "identity")
+        facet_wrap(~ gene_name, ncol = 4) + 
+        ggtitle(paste("Expression level of", gene_names_string)) +
+        ylab("CPM")
+      
     }
   })
-
+#   output$graph <- renderPlot({
+#     place_seedlings <- match(input$gene, seedlings$X, nomatch = 0)
+#     if (place_seedlings == 0){
+#       stop(paste(input$gene, "does not exist"))
+#     }else{
+#       Species <- names(seedlings)[2:5]
+#       CPM <- as.numeric(seedlings[place_seedlings, 2:5])
+#       if (input$logscale == TRUE) {CPM <- log2(CPM)}
+#       combined <- cbind(Species, CPM)
+#       df <- as.data.frame(combined)      
+#       ggplot(df, aes(x = Species, y = CPM)) + 
+#         geom_bar(stat = "identity", position = "identity", aes(fill = Species)) + 
+#         ggtitle(paste("Expression level of", input$gene))
+#     }
+#   })
   
   # produces the data for 1 table, use radio buttons to determine which to plot
   table_data <- reactive({
@@ -98,40 +133,46 @@ shinyServer(function(input, output) {
   
   # determines if there is pairwise significance or not
   output$pairwise_significance <- renderText({
-    place_adjusted <- match(input$gene, adjusted$X, nomatch = 0)
-    if (place_adjusted == 0){
-      stop(paste("Pairwise species comparison data does not exist for", input$gene))
-    }else{
-      sig <- as.numeric(adjusted[place_adjusted, 2:7]) <= 0.05
-      if (all(sig == rep(FALSE, 6))) {
-        "There are no significant pairwise comparisons."
-      }else {
-        sig_places <- which(sig)
-        pairs <- vector()
-        for (i in 1:length(sig_places)){
-          colnum <- sig_places[i] + 1
-          if (i == 1){
-            switch(names(adjusted)[colnum],
-                   SLY_SPI = {pairs <- "S. lycopersicum and S. pimpinellifolium"},
-                   SLY_SHA = {pairs <- "S. lycopersicum and S. habrochaites"},
-                   SLY_SPE = {pairs <- "S. lycopersicum and S. pennellii"},
-                   SPI_SHA = {pairs <- "S. pimpinellifolium and S. habrochaites"},
-                   SPI_SPE = {pairs <- "S. pimpinellifolium and S. pennellii"},
-                   SHA_SPE = {pairs <- "S. habrochaites and S. pennellii"}
-            )}else {
-              switch(names(adjusted)[colnum],
-                     SLY_SPI = {pairs <- paste(pairs, "S. lycopersicum and S. pimpinellifolium", sep = "; ")},
-                     SLY_SHA = {pairs <- paste(pairs, "S. lycopersicum and S. habrochaites", sep = "; ")},
-                     SLY_SPE = {pairs <- paste(pairs, "S. lycopersicum and S. pennellii", sep = "; ")},
-                     SPI_SHA = {pairs <- paste(pairs, "S. pimpinellifolium and S. habrochaites", sep = "; ")},
-                     SPI_SPE = {pairs <- paste(pairs, "S. pimpinellifolium and S. pennellii", sep = "; ")},
-                     SHA_SPE = {pairs <- paste(pairs, "S. habrochaites and S. pennellii", sep = "; ")}
-              )
-            }
-        }
-        paste("There are significant pairwise comparisons for:", pairs)
-      }
+    places <- grep(input$gene, seedlings$X)
+    names <- vector()
+    for (i in 1:length(places)){
+      names[i] <- as.character(seedlings$X[places[i]])
     }
+    names
+#     place_adjusted <- match(input$gene, adjusted$X, nomatch = 0)
+#     if (place_adjusted == 0){
+#       stop(paste("Pairwise species comparison data does not exist for", input$gene))
+#     }else{
+#       sig <- as.numeric(adjusted[place_adjusted, 2:7]) <= 0.05
+#       if (all(sig == rep(FALSE, 6))) {
+#         "There are no significant pairwise comparisons."
+#       }else {
+#         sig_places <- which(sig)
+#         pairs <- vector()
+#         for (i in 1:length(sig_places)){
+#           colnum <- sig_places[i] + 1
+#           if (i == 1){
+#             switch(names(adjusted)[colnum],
+#                    SLY_SPI = {pairs <- "S. lycopersicum and S. pimpinellifolium"},
+#                    SLY_SHA = {pairs <- "S. lycopersicum and S. habrochaites"},
+#                    SLY_SPE = {pairs <- "S. lycopersicum and S. pennellii"},
+#                    SPI_SHA = {pairs <- "S. pimpinellifolium and S. habrochaites"},
+#                    SPI_SPE = {pairs <- "S. pimpinellifolium and S. pennellii"},
+#                    SHA_SPE = {pairs <- "S. habrochaites and S. pennellii"}
+#             )}else {
+#               switch(names(adjusted)[colnum],
+#                      SLY_SPI = {pairs <- paste(pairs, "S. lycopersicum and S. pimpinellifolium", sep = "; ")},
+#                      SLY_SHA = {pairs <- paste(pairs, "S. lycopersicum and S. habrochaites", sep = "; ")},
+#                      SLY_SPE = {pairs <- paste(pairs, "S. lycopersicum and S. pennellii", sep = "; ")},
+#                      SPI_SHA = {pairs <- paste(pairs, "S. pimpinellifolium and S. habrochaites", sep = "; ")},
+#                      SPI_SPE = {pairs <- paste(pairs, "S. pimpinellifolium and S. pennellii", sep = "; ")},
+#                      SHA_SPE = {pairs <- paste(pairs, "S. habrochaites and S. pennellii", sep = "; ")}
+#               )
+#             }
+#         }
+#         paste("There are significant pairwise comparisons for:", pairs)
+#       }
+#     }
   })
   
   output$download_table <- downloadHandler(
@@ -139,5 +180,5 @@ shinyServer(function(input, output) {
     content = function(file) {
       write.csv(table_data(), file)
     }
-)
+  )
 })
